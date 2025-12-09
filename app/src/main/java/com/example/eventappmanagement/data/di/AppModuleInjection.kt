cef99e13bde0c3c0067ba141a3dd7e058a9e322f
vincenciusadyatma <adyatmav@gmail.com>
2025-12-09 23:04:04 +0700

    feat: create AppModuleInjection for initialize retrofit

--- app/src/main/java/com/example/eventappmanagement/data/di/AppModuleInjection.kt
+++ app/src/main/java/com/example/eventappmanagement/data/di/AppModuleInjection.kt
@@ -1,5 +1,21 @@
 package com.example.eventappmanagement.data.di
+import com.example.eventappmanagement.data.remote.api.ApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
-object AppModuleInjection {
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.create
 
+object AppModuleInjection {
+    private val json = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+    }
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("http://104.248.153.158/event-api/")
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .build()
+
+    val eventService: ApiService = retrofit.create()
 }
